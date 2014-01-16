4ec771be6e24604c524d7a17cc586ee86d4cea8d
Zack Bloom <zbloom@hubspot.com>
2014-01-15 23:15:26 -0500

    Add native event triggering

---
+++ comparisons/events/trigger_native/ie8.js
@@ -0,0 +1,7 @@
+if (document.createEvent){
+  event = document.createEvent("HTMLEvents")
+  event.initEvent("change", true, false)
+  el.dispatchEvent(event)
+} else {
+  el.fireEvent('onchange')
+}
---
+++ comparisons/events/trigger_native/ie9.js
@@ -0,0 +1,3 @@
+event = document.createEvent("HTMLEvents")
+event.initEvent("change", true, false)
+el.dispatchEvent(event)
