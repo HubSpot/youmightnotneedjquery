cff9add1c3d25f0f477ef00100a08bbc9fbc3078
Zack Bloom <zbloom@hubspot.com>
2014-01-16 12:42:31 -0500

    Add ready

---
+++ comparisons/events/ready/ie8.js
@@ -0,0 +1,10 @@
+function ready(fn){
+  if (document.addEventListener){
+    document.addEventListener("DOMContentLoaded", fn)
+  } else {
+    document.attachEvent('onreadystatechange', function(){
+      if (document.readyState === 'interactive')
+        fn();
+    });
+  }
+}
