64be771af9266c41074c9ad0230acafab20e3ab2
Zack Bloom <zbloom@hubspot.com>
2014-01-30 17:58:24 -0500

    Fix fade in in ie8.  Closes #11.

--- comparisons/effects/fade_in/ie9.js
+++ comparisons/effects/fade_in/ie9.js
@@ -1,18 +1,12 @@
 function fadeIn(el) {
-  var opacity = 0
-
   el.style.opacity = 0
-  el.style.filter = ''
 
   var last = +new Date
   var tick = function() {
-    opacity += (new Date - last) / 400
-    el.style.opacity = opacity
-    el.style.filter = 'alpha(opacity=' + (100 * opacity)|0 + ')'
-
+    el.style.opacity += (new Date - last) / 400
     last = +new Date
 
-    if (opacity < 1)
+    if (el.style.opacity < 1)
       (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16)
   }
 
