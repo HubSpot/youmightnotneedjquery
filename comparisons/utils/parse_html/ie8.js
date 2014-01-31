229291d074d4c7a06ccb1af1f4758c63f478ba38
Zack Bloom <zbloom@hubspot.com>
2014-01-31 03:11:37 -0500

    Remove secure ie8 version, as jq does not have any such security.

--- comparisons/utils/parse_html/ie8.js
+++ comparisons/utils/parse_html/ie8.js
@@ -1,13 +1,3 @@
-// This version is save, but uses ActiveX
-var parseHTML = function(str) {
-  var tmp = new ActiveXObject('htmlfile');
-  tmp.open();
-  tmp.write(str);
-  tmp.close();
-  return tmp.body.children;
-};
-
-// This version is prone to DOM XSS, only use with caution!
 var parseHTML = function(str) {
   var el = document.createElement('div');
   el.innerHTML = str;
