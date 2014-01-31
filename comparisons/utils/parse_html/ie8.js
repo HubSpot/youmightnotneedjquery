d41c6ad24b6f6071e0633d2038438f4fbab3b77a
Frederic Hemberger <mail@frederic-hemberger.de>
2014-01-31 09:06:34 +0100

    Update ie8.js

--- comparisons/utils/parse_html/ie8.js
+++ comparisons/utils/parse_html/ie8.js
@@ -1,3 +1,4 @@
+// This version is save, but uses ActiveX
 var parseHTML = function(str) {
   var tmp = new ActiveXObject('htmlfile');
   tmp.open();
@@ -5,5 +6,12 @@ var parseHTML = function(str) {
   tmp.close();
   return tmp.body.children;
 };
+
+// This version is prone to DOM XSS, only use with caution!
+var parseHTML = function(str) {
+  var el = document.createElement('div');
+  el.innerHTML = str;
+  return el.children;
+}
 
 parseHTML(htmlString);
