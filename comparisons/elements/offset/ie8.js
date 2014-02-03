71717a315a04a0a176f49d338c2bf9b6ab71a8c1
Nick Beeuwsaert <nick.beeuwsaert@gmail.com>
2014-02-02 20:44:19 -0600

    made offset work with elements out of the normal page flow

--- comparisons/elements/offset/ie8.js
+++ comparisons/elements/offset/ie8.js
@@ -1,1 +1,9 @@
-{left: el.offsetLeft, top: el.offsetTop}
+function offset(el){
+  var offset = {top: 0, left: 0};
+    while(el != undefined){
+        offset.left += el.offsetLeft;
+        offset.top += el.offsetTop;
+        el = el.offsetParent;
+    }
+    return offset;
+}
