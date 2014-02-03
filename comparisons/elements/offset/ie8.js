219861b42b78c9ba270cfafcd223761e4bc64edd
Nick Beeuwsaert <nick.beeuwsaert@gmail.com>
2014-02-03 06:48:14 -0600

    updated code to use elem.getBoundingClientRect() as suggested by @7cc

--- comparisons/elements/offset/ie8.js
+++ comparisons/elements/offset/ie8.js
@@ -1,9 +1,8 @@
 function offset(el){
-  var offset = {top: 0, left: 0};
-    while(el != undefined){
-        offset.left += el.offsetLeft;
-        offset.top += el.offsetTop;
-        el = el.offsetParent;
-    }
-    return offset;
+box = el.getBoundingClientRect();
+docElem = document.documentElement;
+return {
+        top: box.top  + ( window.pageYOffset || docElem.scrollTop )  - ( docElem.clientTop  || 0 ),
+        left: box.left + ( window.pageXOffset || docElem.scrollLeft ) - ( docElem.clientLeft || 0 )
+    };
 }
