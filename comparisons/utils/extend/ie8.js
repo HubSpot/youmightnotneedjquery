86fa2fb08b055a6a82e0aeb57156a3f2c918e654
tomByrer <tomByrer@gmail.com>
2014-02-04 23:48:38 -0700

    -{}

--- comparisons/utils/extend/ie8.js
+++ comparisons/utils/extend/ie8.js
@@ -2,14 +2,12 @@ var extend = function(out) {
   out = out || {};
 
   for (var i = 1; i < arguments.length; i++) {
-    if (!arguments[i]) {
+    if (!arguments[i])
       continue;
-    }
 
     for (var key in arguments[i]) {
-      if (arguments[i].hasOwnProperty(key)) {
+      if (arguments[i].hasOwnProperty(key))
         out[key] = arguments[i][key];
-      }
     }
   }
 
