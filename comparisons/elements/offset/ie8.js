c60c8ddd6f0fe1392277950a11c42505f7d4f56a
Nick Beeuwsaert <nick.beeuwsaert@gmail.com>
2014-02-05 10:43:26 -0600

    Created a ie9.js, removed useless calls

--- comparisons/elements/offset/ie8.js
+++ comparisons/elements/offset/ie8.js
@@ -1,8 +1,8 @@
 function offset(el){
-box = el.getBoundingClientRect();
-docElem = document.documentElement;
-return {
-        top: box.top  + ( window.pageYOffset || docElem.scrollTop )  - ( docElem.clientTop  || 0 ),
-        left: box.left + ( window.pageXOffset || docElem.scrollLeft ) - ( docElem.clientLeft || 0 )
+    box = el.getBoundingClientRect();
+    docElem = document.documentElement;
+    return {
+        top: box.top  + (window.pageYOffset || docElem.scrollTop),
+        left: box.left + (window.pageXOffset || docElem.scrollLeft)
     };
 }
