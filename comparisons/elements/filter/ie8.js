04de7f1b4f46f9018b450e0a45b69ca867c8fd74
tomByrer <tomByrer@gmail.com>
2014-02-04 23:37:33 -0700

    -{}

--- comparisons/elements/filter/ie8.js
+++ comparisons/elements/filter/ie8.js
@@ -2,9 +2,8 @@ function filter(selector, filterFn) {
   var elements = document.querySelectorAll(selector);
   var out = [];
   for (var i = elements.length; i--;) {
-    if (filterFn(elements[i])) {
+    if (filterFn(elements[i]))
       out.unshift(elements[i]);
-    }
   }
   return out;
 }
