8378b0f1d468ef4f09bdf7c8b322ef4ac55ea74a
vicnicius <vchavesster@gmail.com>
2014-02-06 01:01:02 -0200

    #41
    Adding .outerHeight & .outerWidth

---
+++ comparisons/utils/outer_width/ie8.js
@@ -0,0 +1,21 @@
+function outerWidth(el, includeMargin){
+
+    var includeMargin = includeMargin || false;
+
+    //the computed width of the element, with borders and padding, but without margin.
+    var width = el.offsetWidth;
+
+    if(includeMargin){
+
+      var style = el.currentStyle || window.getComputedStyle(el);
+
+      //adding margin
+      width += parseInt(style.marginTop) + parseInt(style.marginBottom);
+
+    } 
+
+    return width;
+
+  }
+
+  outerWidth(el, true);
