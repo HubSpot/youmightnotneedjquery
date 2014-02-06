e949c73887ac270fc463263bf1f35d4f46d7d9d5
Zack Bloom <zbloom@hubspot.com>
2014-02-05 23:50:20 -0500

    Cleanup

--- comparisons/elements/outer_height/ie9.js
+++ comparisons/elements/outer_height/ie9.js
@@ -2,7 +2,7 @@ function outerHeight(el, includeMargin){
   var height = el.offsetHeight;
 
   if(includeMargin){
-    var style = el.currentStyle || getComputedStyle(el);
+    var style = getComputedStyle(el);
     height += parseInt(style.marginTop) + parseInt(style.marginBottom);
   } 
   return height;
