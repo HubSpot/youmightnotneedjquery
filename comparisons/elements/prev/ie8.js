2f06fed841569be6e878c968e13642705cbfdf71
Zack Bloom <zbloom@hubspot.com>
2014-02-12 04:12:04 -0500

    Fix incorrect property name.  Closes #53.

--- comparisons/elements/prev/ie8.js
+++ comparisons/elements/prev/ie8.js
@@ -1,7 +1,7 @@
 // prevSibling can include text nodes
-function prevElementSibling(el) {
-  do { el = el.prevSibling; } while ( el && el.nodeType !== 1 );
+function previousElementSibling(el) {
+  do { el = el.previousSibling; } while ( el && el.nodeType !== 1 );
   return el;
 }
 
-el.prevElementSibling || prevElementSibling(el);
+el.previousElementSibling || previousElementSibling(el);
--- comparisons/elements/prev/ie9.js
+++ comparisons/elements/prev/ie9.js
@@ -1,1 +1,1 @@
-el.prevElementSibling
+el.previousElementSibling
