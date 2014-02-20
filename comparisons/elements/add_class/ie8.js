464303d28971a9fd3c1762cd30ce2ff7d03610e6
Krasimir Tsonev <krasimir@outset.ws>
2014-02-20 18:31:58 +0200

    Completing the 'add class' snippet

--- comparisons/elements/add_class/ie8.js
+++ comparisons/elements/add_class/ie8.js
@@ -1,4 +1,9 @@
-if (el.classList)
+if(el.classList && false) {
   el.classList.add(className);
-else
-  el.className += ' ' + className;
+} else {
+  var current = el.className;
+  if(current.indexOf(className) < 0) {
+    if(current == '') el.className = className;
+    else el.className += ' ' + className;
+  }
+}
