eb69c1f66e7b110ec6fc52a236c1e78226d30d0e
Krasimir Tsonev <krasimir@outset.ws>
2014-06-26 16:20:23 +0300

    Replace the usage of Array.indexOf

--- comparisons/elements/add_class/ie8.js
+++ comparisons/elements/add_class/ie8.js
@@ -1,8 +1,10 @@
 if (el.classList) {
   el.classList.add(className);
 } else {
-  var current = el.className;
-  if(current.split(' ').indexOf(className) < 0) {
+  var current = el.className, found = false;
+  var all = current.split(' ');
+  for(var i=0; i<all.length, !found; i++) found = all[i] === className;
+  if(!found) {
     if(current === '') el.className = className;
     else el.className += ' ' + className;
   }
