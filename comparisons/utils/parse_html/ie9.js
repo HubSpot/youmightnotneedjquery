334d10fbc102cf5638592613c97fa6870f598681
Nathan Black <nathan@nathanblack.org>
2014-11-06 11:47:43 -0800

    IE throws if createHTMLDocument doesn't have a title

--- comparisons/utils/parse_html/ie9.js
+++ comparisons/utils/parse_html/ie9.js
@@ -1,5 +1,5 @@
 var parseHTML = function(str) {
-  var tmp = document.implementation.createHTMLDocument();
+  var tmp = document.implementation.createHTMLDocument('title');
   tmp.body.innerHTML = str;
   return tmp.body.children;
 };
