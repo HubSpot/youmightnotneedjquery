6c93c17d4928e16149592855d04bf927d7bf7fd8
Some Web Media <somewebmedia@gmail.com>
2014-11-22 18:42:51 +0100

    outerHeight with margins fix
    When margin is set to auto (margin:auto) my IE9 returned 'auto' as a
    value so parseInt was NaN, so the function returned NaN. This way
    returned value will be zero as it should.

--- comparisons/elements/outer_height_with_margin/ie9.js
+++ comparisons/elements/outer_height_with_margin/ie9.js
@@ -2,7 +2,7 @@ function outerHeight(el){
   var height = el.offsetHeight;
   var style = getComputedStyle(el);
 
-  height += parseInt(style.marginTop) + parseInt(style.marginBottom);
+  height += (parseInt(style.marginTop) || 0) + (parseInt(style.marginBottom) || 0);
   return height;
 }
 
