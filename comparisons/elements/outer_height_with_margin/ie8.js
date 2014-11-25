6050ebfa3aba4a8725f272189ef021115fd60d52
Some Web Media <somewebmedia@gmail.com>
2014-11-25 23:12:41 +0100

    Added brackets around properties values
    Seems however it doesn't work without brackets, my bad.

--- comparisons/elements/outer_height_with_margin/ie8.js
+++ comparisons/elements/outer_height_with_margin/ie8.js
@@ -2,7 +2,7 @@ function outerHeight(el){
   var height = el.offsetHeight;
   var style = el.currentStyle || getComputedStyle(el);
 
-  height += parseInt(style.marginTop) || 0 + parseInt(style.marginBottom) || 0;
+  height += (parseInt(style.marginTop) || 0) + (parseInt(style.marginBottom) || 0);
   return height;
 }
 
