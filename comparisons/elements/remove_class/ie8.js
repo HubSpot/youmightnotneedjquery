47fd284ff3e9fa73338a9703286d972fc29a189b
Daiwei <daiweilu123@gmail.com>
2015-01-28 17:57:03 -0500

    Improve removeClass

--- comparisons/elements/remove_class/ie8.js
+++ comparisons/elements/remove_class/ie8.js
@@ -1,4 +1,16 @@
-if (el.classList)
+if (el.classList) {
   el.classList.remove(className);
-else
-  el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+} else {
+  var cur = (' ' + el.className + ' ')
+    , name = ' ' + className + ' ';
+
+  while (cur.indexOf(name) > -1) {
+    cur = cur.replace(name, ' ');
+  }
+
+  // Trim white spaces
+  var finalValue = cur.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
+  if (el.className !== finalValue) {
+    el.className = finalValue;
+  }
+}
