ff5ef015511e39e64be7a757d13b998d9263c232
camsong <neosoyn@gmail.com>
2015-02-26 18:36:17 +0800

    rollback to '0' if parseInt got 'NaN'

--- comparisons/elements/outer_width_with_margin/ie8.js
+++ comparisons/elements/outer_width_with_margin/ie8.js
@@ -2,7 +2,7 @@ function outerWidth(el) {
   var width = el.offsetWidth;
   var style = el.currentStyle || getComputedStyle(el);
 
-  width += parseInt(style.marginLeft) + parseInt(style.marginRight);
+  width += (parseInt(style.marginLeft) || 0) + (parseInt(style.marginRight) || 0);
   return width;
 }
 
