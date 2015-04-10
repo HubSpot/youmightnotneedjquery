fbd90973c8d397a847c4fbd42b8535b272181fd9
Vageesh Dwivedi <vageesh.dwivedi@247-inc.com>
2015-04-10 06:00:53 -0700

    fix: ie8 regex in removeClass

--- comparisons/elements/remove_class/ie8.js
+++ comparisons/elements/remove_class/ie8.js
@@ -1,4 +1,4 @@
 if (el.classList)
   el.classList.remove(className);
 else
-  el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+  el.className = el.className.replace(new RegExp('(^|\\b)(\\s+)' + className.split(' ').join('(\\s+)|(\\s+)') + '(\\s+)(\\b|$|\s+$)', 'g'), ' ');
