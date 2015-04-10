af7e8f29fdf2d968f196acba317711d24f13546a
Vageesh Dwivedi <vageesh.dwivedi@247-inc.com>
2015-04-10 07:10:31 -0700

    fix: ie8 regex spaces

--- comparisons/elements/remove_class/ie8.js
+++ comparisons/elements/remove_class/ie8.js
@@ -1,4 +1,4 @@
 if (el.classList)
   el.classList.remove(className);
 else
-  el.className = el.className.replace(new RegExp('(^|\\b)(\\s+)' + className.split(' ').join('(\\s+)|(\\s+)') + '(\\s+)(\\b|$)', 'g'), ' ');
+  el.className = el.className.replace(new RegExp('(^|\\b)(\\s*)' + className.split(' ').join('(\\s*)|(\\s*)') + '(\\s*)(\\b|$)', 'g'), ' ');
