d23a62ee7e3fc4b6b603b7c74e637b2f6c04b8c8
immeëmosol <will+developer@willfris.nl>
2015-08-21 15:34:15 +0200

    Update ie8.js
    With this update className = 'no-js' no longer removes the 'no-js' part from an element.className that contains 'hey-no-js' .
    This update removes exuberant whitespace-characters, works over multiple lines (in case someone did that) both from el.className & from give className-param.

--- comparisons/elements/remove_class/ie8.js
+++ comparisons/elements/remove_class/ie8.js
@@ -1,4 +1,16 @@
 if (el.classList)
   el.classList.remove(className);
 else
-  el.className = el.className.replace(new RegExp('(^|\\b)(\\s*)' + className.split(' ').join('(\\s*)|(\\s*)') + '(\\s*)(\\b|$)', 'g'), ' ');
+  el.className = el.className.replace(
+      new RegExp( '(^|\\s)('
+        + className.split( \s ).filter(
+          function ( e ) { return e; }
+        ).join( '|' )
+        + ')(\\s|$)' , 'gm'
+      )
+      , ' '
+  ).split( ' ' ).filter(
+    function ( e ) { return e; }
+  ).join( ' ' );
+// dependent on filter, see polyFill for instance @ MDN :
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter#Polyfill .
