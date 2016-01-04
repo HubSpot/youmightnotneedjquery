3cea8a6fdbbf548a8888b7f51c1740b816dca65d
Phil Helm <philip.helm@omgplc.com>
2016-01-04 14:37:05 +0000

    fixes deep extend bug where nested objects are not copied

--- comparisons/utils/deep_extend/ie8.js
+++ comparisons/utils/deep_extend/ie8.js
@@ -4,13 +4,15 @@ var deepExtend = function(out) {
   for (var i = 1; i < arguments.length; i++) {
     var obj = arguments[i];
 
-    if (!obj)
+    if (obj == null)
       continue;
 
     for (var key in obj) {
       if (obj.hasOwnProperty(key)) {
-        if (typeof obj[key] === 'object')
-          deepExtend(out[key], obj[key]);
+        if (obj[key] == null)
+          out[key] = null;
+        else if (typeof obj[key] === 'object')
+          out[key] = deepExtend(out[key], obj[key]);
         else
           out[key] = obj[key];
       }
