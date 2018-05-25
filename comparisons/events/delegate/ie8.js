ac6a5f7fc19a9f4e3649970b392824fddfe4f1ff
Luiz Machado <machado@odahcam.com>
2018-05-25 09:41:28 -0300

    add var names

--- comparisons/events/delegate/ie8.js
+++ comparisons/events/delegate/ie8.js
@@ -1,7 +1,7 @@
-document.addEventListener('click', function(e) {
+document.addEventListener(eventName, function(e) {
     // loop parent nodes from the target to the delegation node
     for (var target = e.target; target && target != this; target = target.parentNode) {
-        if (target.matches(<selector>)) {
+        if (target.matches(elementSelector)) {
             handler.call(target, e);
             break;
         }
