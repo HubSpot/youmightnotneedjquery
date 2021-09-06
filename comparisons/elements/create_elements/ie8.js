285fd88902405f395af8f2f03ca98dd2f7c8d1e6
Peter Abbondanzo <peter@abbondanzo.com>
2021-09-06 11:30:22 -0400

    Formatting fixes
    - Cleaned up spaces around `if` blocks
    - Replaced abstract equality with strict equality
    - Named `generateElements` function

--- comparisons/elements/create_elements/ie8.js
+++ comparisons/elements/create_elements/ie8.js
@@ -1,10 +1,10 @@
-var generateElements = function(text) {
+function generateElements(text) {
   var div = document.createElement('div');
   div.innerHTML = txt;
-  if(div.children.length == 0) {
+  if (div.children.length === 0) {
     return null;
   }
-  if( div.children.length == 1 ) {
+  if (div.children.length === 1) {
     return div.firstChild;
   } else {
     return div.children;
