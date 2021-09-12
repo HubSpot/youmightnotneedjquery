6b5ad0874de1e9fcf6182c4112500f2bea3dc63d
Richie Bendall <richiebendall@gmail.com>
2021-09-12 19:56:12 +1200

    Update comparisons/utils/is_numeric/ie8.js
    Co-authored-by: Peter Abbondanzo <peter@abbondanzo.com>

--- comparisons/utils/is_numeric/ie8.js
+++ comparisons/utils/is_numeric/ie8.js
@@ -1,4 +1,4 @@
-function isNumeric(val) {    
+function isNumeric(num) {    
     if (typeof num === 'number') return num - num === 0;
     if (typeof num === 'string' && num.trim() !== '') return Number.isFinite ? Number.isFinite(+num) : isFinite(+num);
     return false;
