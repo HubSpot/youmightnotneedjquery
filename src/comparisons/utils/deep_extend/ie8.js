91adcf9d8345d8e252c25d0a541ecf95adfb6bcc
Peter Abbondanzo <peter@abbondanzo.com>
2021-09-12 03:38:41 -0400

    Add null check to deep extend (#238)

--- src/comparisons/utils/deep_extend/ie8.js
+++ src/comparisons/utils/deep_extend/ie8.js
@@ -1,22 +1,17 @@
-var deepExtend = function(out) {
+var deepExtend = function (out) {
   out = out || {};
 
   for (var i = 1; i < arguments.length; i++) {
     var obj = arguments[i];
 
-    if (!obj)
-      continue;
+    if (!obj) continue;
 
     for (var key in obj) {
       if (obj.hasOwnProperty(key)) {
-        if (typeof obj[key] === 'object'){
-          if(obj[key] instanceof Array == true)
-            out[key] = obj[key].slice(0);
-          else
-            out[key] = deepExtend(out[key], obj[key]);
-        }
-        else
-          out[key] = obj[key];
+        if (typeof obj[key] === "object" && obj[key] !== null) {
+          if (obj[key] instanceof Array) out[key] = obj[key].slice(0);
+          else out[key] = deepExtend(out[key], obj[key]);
+        } else out[key] = obj[key];
       }
     }
   }
