7825dba4d08fa4db5d4ee5bf94440f06e57f25b1
Peter Abbondanzo <peter@abbondanzo.com>
2022-08-27 12:00:22 -0400

    Allow custom speeds in the fadeIn, fadeOut functions (#252)

--- src/comparisons/effects/fade_out/ie9.js
+++ src/comparisons/effects/fade_out/ie9.js
@@ -1,13 +1,15 @@
-function fadeOut(el) {
+function fadeOut(el, speed = 400) {
   el.style.opacity = 1;
 
   var last = +new Date();
-  var tick = function() {
-    el.style.opacity = +el.style.opacity - (new Date() - last) / 400;
+  var tick = function () {
+    el.style.opacity = +el.style.opacity - (new Date() - last) / speed;
+    if (opacity < 0) opacity = 0;
     last = +new Date();
 
     if (+el.style.opacity > 0) {
-      (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+      (window.requestAnimationFrame && requestAnimationFrame(tick)) ||
+        setTimeout(tick, 16);
     }
   };
 
