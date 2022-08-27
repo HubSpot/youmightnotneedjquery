52ce200690744531aa6938ca5f15cc9590208eeb
Abbondanzo <peter@abbondanzo.com>
2022-08-27 12:56:14 -0400

    Clean up ajax snippets

--- src/comparisons/ajax/request/ie9.js
+++ src/comparisons/ajax/request/ie9.js
@@ -1,18 +1,20 @@
-var request = new XMLHttpRequest();
-request.open('GET', '/my/url', true);
+function request(success, error) {
+  var request = new XMLHttpRequest();
+  request.open('GET', '/my/url', true);
 
-request.onload = function() {
-  if (request.status >= 200 && request.status < 400) {
-    // Success!
-    var resp = request.responseText;
-  } else {
-    // We reached our target server, but it returned an error
+  request.onload = function () {
+    if (this.status >= 200 && this.status < 400) {
+      // Success! If you expect this to be JSON, use JSON.parse!
+      success(this.responseText, this.status);
+    } else {
+      // We reached our target server, but it returned an error
+      error();
+    }
+  };
 
-  }
-};
+  request.onerror = function () {
+    error();
+  };
 
-request.onerror = function() {
-  // There was a connection error of some sort
-};
-
-request.send();
+  request.send();
+}
