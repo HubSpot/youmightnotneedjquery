ec6e4c2a5951af9cb1e9397efcc83bf4ed4dd547
Peter Abbondanzo <peter@abbondanzo.com>
2022-08-27 13:49:27 -0400

    Add example with descendent event handler delegates (#262)

--- src/comparisons/events/on/ie9.js
+++ src/comparisons/events/on/ie9.js
@@ -1,1 +1,15 @@
-el.addEventListener(eventName, eventHandler);
+function addEventListener(el, eventName, eventHandler, selector) {
+  if (selector) {
+    el.addEventListener(eventName, function (e) {
+      if (e.target && e.target.matches(selector)) {
+        eventHandler(e);
+      }
+    });
+  } else {
+    el.addEventListener(eventName, eventHandler);
+  }
+}
+
+addEventListener(el, eventName, eventHandler);
+// Or when you want to delegate event handling
+addEventListener(el, eventName, eventHandler, selector);
