0663dc3a3622f02069cc55d0b7fab759e9355d5e
Peter Abbondanzo <peter@abbondanzo.com>
2022-08-27 12:57:51 -0400

    Clean up ajax snippets (#256)

--- src/comparisons/ajax/request/ie8.js
+++ src/comparisons/ajax/request/ie8.js
@@ -1,16 +1,17 @@
-var request = new XMLHttpRequest();
-request.open('GET', '/my/url', true);
+function request(success, error) {
+  var request = new XMLHttpRequest();
+  request.open('GET', '/my/url', true);
 
-request.onreadystatechange = function() {
-  if (this.readyState === 4) {
-    if (this.status >= 200 && this.status < 400) {
-      // Success!
-      var resp = this.responseText;
-    } else {
-      // Error :(
+  request.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      if (this.status >= 200 && this.status < 400) {
+        // Success! If you expect this to be JSON, use JSON.parse!
+        success(this.responseText, this.status);
+      } else {
+        error();
+      }
     }
-  }
-};
+  };
 
-request.send();
-request = null;
+  request.send();
+}
