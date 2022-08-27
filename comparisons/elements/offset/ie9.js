462ccdfbb45f4ba4afc88ce06334c514aa755a1c
Peter Abbondanzo <peter@abbondanzo.com>
2022-08-27 11:42:27 -0400

    made offset work with elements out of the normal page flow (#251)
    * made offset work with elements out of the normal page flow
    
    * updated code to use elem.getBoundingClientRect() as suggested by @7cc
    
    * Created a ie9.js, removed useless calls
    
    Co-authored-by: Nick Beeuwsaert <nick.beeuwsaert@gmail.com>

---
+++ comparisons/elements/offset/ie9.js
@@ -0,0 +1,8 @@
+function offset(el){
+    box = el.getBoundingClientRect();
+    docElem = document.documentElement;
+    return {
+        top: box.top + window.pageYOffset - docElem.clientTop,
+        left: box.left + window.pageXOffset - docElem.clientLeft
+    };
+}
--- src/comparisons/elements/position/ie8.js
+++ src/comparisons/elements/position/ie8.js
@@ -1,1 +1,8 @@
-{left: el.offsetLeft, top: el.offsetTop}
+function offset(el){
+    box = el.getBoundingClientRect();
+    docElem = document.documentElement;
+    return {
+        top: box.top  + (window.pageYOffset || docElem.scrollTop),
+        left: box.left + (window.pageXOffset || docElem.scrollLeft)
+    };
+}
