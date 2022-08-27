e6a9c05c9ddef95d1c16ea458a0521c603fc9ca3
Peter Abbondanzo <peter@abbondanzo.com>
2022-08-27 13:30:10 -0400

    Migrate comparisons to the correct folder (#260)

--- comparisons/events/click/ie8.js
+++
@@ -1,7 +0,0 @@
-if (document.createEvent) {
-  var event = document.createEvent('HTMLEvents');
-  event.initEvent('click', true, false);
-  el.dispatchEvent(event);
-} else {
-  el.fireEvent('onclick');
-}
