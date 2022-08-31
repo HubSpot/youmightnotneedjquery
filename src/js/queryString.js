f1e43f30e9c4fdadb547a7d454215bf1fe9b5aa0
Michael Scott Asato Cuthbert <cuthbert@mit.edu>
2022-08-30 16:59:36 -1000

    Add ES6 slider option (#271)
    * Add ES6 slider option
    
    Add an option for ES6 slider -- can think of other things to rename to though (some keywords such as `await` didn't arrive until ES2016 or later).
    
    Fixes the main issue in #222 but only implements one comparison example (JSON) as a demonstration.
    
    Tested that old-ie message should display, but didn't test it on an actual Internet Explorer browser.
    
    * Freudian slip

--- src/js/queryString.js
+++ src/js/queryString.js
@@ -1,10 +1,22 @@
 function setQueryString(supportedVersion) {
+  let supportedVersionStr = 'ie' + supportedVersion.toString();
+  if (supportedVersion === 12) {
+    supportedVersionStr = 'es6';
+  }
+
   const urlParams = new URLSearchParams(location.search);
-  urlParams.set('support', supportedVersion);
+  urlParams.set('support', supportedVersionStr);
   history.replaceState({}, '', '?' + urlParams);
 }
 
 function getInitialSliderState() {
   const urlParams = new URLSearchParams(location.search);
-  return urlParams.get('support');
+  let supportVersionStr = urlParams.get('support');
+  let supportVersion = 12;
+  if (supportVersionStr === 'es6') {
+    supportVersion = 12;
+  } else if (supportVersionStr && supportVersionStr.indexOf('ie') === 0) {
+    supportVersion = parseInt(supportVersionStr.substring(2));
+  }
+  return supportVersion;
 }
