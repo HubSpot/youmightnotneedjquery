8df68c8c1dfdcea1af02bc58f3991ac0785f134b
Peter Abbondanzo <peter@abbondanzo.com>
2022-09-01 23:14:00 -0400

    Rename all es6.js files to modern.js (#279)
    * Rename all files es6 -> modern
    
    * Rename es6 -> modern
    
    Co-authored-by: Peter Abbondanzo <pabbondanzo@hubspot.com>

--- src/js/queryString.js
+++ src/js/queryString.js
@@ -1,7 +1,7 @@
 function setQueryString(supportedVersion) {
   let supportedVersionStr = 'ie' + supportedVersion.toString();
   if (supportedVersion === 12) {
-    supportedVersionStr = 'es6';
+    supportedVersionStr = 'modern';
   }
 
   const urlParams = new URLSearchParams(location.search);
@@ -13,7 +13,7 @@ function getInitialSliderState() {
   const urlParams = new URLSearchParams(location.search);
   let supportVersionStr = urlParams.get('support');
   let supportVersion = 12;
-  if (supportVersionStr === 'es6') {
+  if (supportVersionStr === 'modern') {
     supportVersion = 12;
   } else if (supportVersionStr && supportVersionStr.indexOf('ie') === 0) {
     supportVersion = parseInt(supportVersionStr.substring(2));
