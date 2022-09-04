2f02bdbafdcbe350583e3354397358cc631acd72
Richie Bendall <richiebendall@gmail.com>
2022-09-04 21:32:12 +1200

    Compress build
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- astro.config.ts
+++ astro.config.ts
@@ -1,3 +1,6 @@
 import {defineConfig} from 'astro/config';
+import compress from "astro-compress";
 
-export default defineConfig({});
+export default defineConfig({
+  integrations: [compress()]
+});
