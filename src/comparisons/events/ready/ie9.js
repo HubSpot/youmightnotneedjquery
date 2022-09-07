5e245b8ab939a04d695a56731d8c457195b3f058
Abbondanzo <peter@abbondanzo.com>
2022-09-07 00:23:09 -0400

    Fix <Ie10 implementation for ready

--- src/comparisons/events/ready/ie9.js
+++ src/comparisons/events/ready/ie9.js
@@ -1,5 +1,9 @@
 function ready(fn) {
-  if (document.readyState != 'loading') {
+  if (
+    document.attachEvent
+      ? document.readyState === 'complete'
+      : document.readyState !== 'loading'
+  ) {
     fn();
   } else {
     document.addEventListener('DOMContentLoaded', fn);
