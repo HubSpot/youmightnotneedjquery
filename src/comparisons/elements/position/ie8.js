1015f5c61aad7aa67c1c76a52afc0e8c1e1f1756
Peter Abbondanzo <peter@abbondanzo.com>
2022-09-15 22:18:05 -0400

    Position fix (#294)
    * Fix position calculations to take margin into account
    
    * Skip empty string examples
    
    * Even more concise

--- src/comparisons/elements/position/ie8.js
+++ src/comparisons/elements/position/ie8.js
@@ -1,8 +1,22 @@
-function offset(el) {
-  box = el.getBoundingClientRect();
-  docElem = document.documentElement;
+function position(el) {
+  var box = el.getBoundingClientRect();
+  var docElem = document.documentElement;
+  var marginLeft = 0;
+  var marginTop = 0;
+  if (typeof getComputedStyle === 'function') {
+    var style = window.getComputedStyle(el);
+    marginLeft = parseInt(style.marginLeft, 10);
+    marginTop = parseInt(style.marginTop, 10);
+  } else if (el.currentStyle) {
+    marginLeft = el.currentStyle['marginLeft']
+      ? parseInt(el.currentStyle['marginLeft'], 10)
+      : 0;
+    marginTop = el.currentStyle['marginTop']
+      ? parseInt(el.currentStyle['marginTop'], 10)
+      : 0;
+  }
   return {
-    top: box.top + (window.pageYOffset || docElem.scrollTop),
-    left: box.left + (window.pageXOffset || docElem.scrollLeft)
+    top: box.top + (window.pageYOffset || docElem.scrollTop) - marginTop,
+    left: box.left + (window.pageXOffset || docElem.scrollLeft) - marginLeft
   };
 }
--- src/comparisons/elements/position/modern.js
+++ src/comparisons/elements/position/modern.js
@@ -1,1 +1,8 @@
-const {top, left} = el.getBoundingClientRect();
+function position(el) {
+  const {top, left} = el.getBoundingClientRect();
+  const {marginTop, marginLeft} = getComputedStyle(el);
+  return {
+    top: top - parseInt(marginTop, 10),
+    left: left - parseInt(marginLeft, 10)
+  };
+}
