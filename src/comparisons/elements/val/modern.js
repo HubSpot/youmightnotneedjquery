951545cacf45746049aa7761e9e0cee3ad0dbe40
Peter Abbondanzo <peter@abbondanzo.com>
2022-09-15 23:05:20 -0400

    Add $.val (#296)

---
+++ src/comparisons/elements/val/modern.js
@@ -0,0 +1,9 @@
+function val(el) {
+  if (el.options && el.multiple) {
+    return el.options
+      .filter((option) => option.selected)
+      .map((option) => option.value);
+  } else {
+    return el.value;
+  }
+}
