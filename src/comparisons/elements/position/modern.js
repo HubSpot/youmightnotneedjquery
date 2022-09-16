ba49d268ae847abd7e7066a4be3ea9b5456607aa
Abbondanzo <peter@abbondanzo.com>
2022-09-15 22:16:08 -0400

    Fix position calculations to take margin into account

--- src/comparisons/elements/position/modern.js
+++ src/comparisons/elements/position/modern.js
@@ -1,1 +1,8 @@
-const {top, left} = el.getBoundingClientRect();
+function position(el) {
+  const {top, left} = el.getBoundingClientRect();
+  const {marginTop, marginLeft} = window.getComputedStyle(el);
+  return {
+    top: top - parseInt(marginTop, 10),
+    left: left - parseInt(marginLeft, 10)
+  };
+}
