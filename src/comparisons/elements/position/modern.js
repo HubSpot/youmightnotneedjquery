0d321949bd687d98eef43a2a505fc0fab18a6bea
Abbondanzo <peter@abbondanzo.com>
2022-09-15 22:16:50 -0400

    Even more concise

--- src/comparisons/elements/position/modern.js
+++ src/comparisons/elements/position/modern.js
@@ -1,6 +1,6 @@
 function position(el) {
   const {top, left} = el.getBoundingClientRect();
-  const {marginTop, marginLeft} = window.getComputedStyle(el);
+  const {marginTop, marginLeft} = getComputedStyle(el);
   return {
     top: top - parseInt(marginTop, 10),
     left: left - parseInt(marginLeft, 10)
