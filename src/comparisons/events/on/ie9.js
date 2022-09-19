b36731144904785f32d97e5be09b91deef396d82
Peter Abbondanzo <peter@abbondanzo.com>
2022-09-19 00:05:26 -0400

    Add escape mechanism for detaching event handlers (#300)
    * Add escape mechanism for detaching event handlers
    
    * Code comment for modern

--- src/comparisons/events/on/ie9.js
+++ src/comparisons/events/on/ie9.js
@@ -1,15 +1,19 @@
 function addEventListener(el, eventName, eventHandler, selector) {
   if (selector) {
-    el.addEventListener(eventName, function (e) {
+    var wrappedHandler = function (e) {
       if (e.target && e.target.matches(selector)) {
         eventHandler(e);
       }
-    });
+    };
+    el.addEventListener(eventName, wrappedHandler);
+    return wrappedHandler;
   } else {
     el.addEventListener(eventName, eventHandler);
+    return eventHandler;
   }
 }
 
+// Use the return value to remove that event listener, see #off
 addEventListener(el, eventName, eventHandler);
 // Or when you want to delegate event handling
 addEventListener(el, eventName, eventHandler, selector);
