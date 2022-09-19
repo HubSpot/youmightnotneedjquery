404cedbcd0e96ab7f1a10b173060bf25be727f3d
Peter Abbondanzo <peter@abbondanzo.com>
2022-09-19 01:05:34 -0400

    Fix up trigger examples to fire native actions (#301)
    * Fix up trigger examples to fire native actions
    
    * Initialize event correctly

--- src/comparisons/events/trigger_native/modern.js
+++ src/comparisons/events/trigger_native/modern.js
@@ -1,3 +1,15 @@
+function trigger(el, eventType) {
+  if (typeof eventType === 'string' && typeof el[eventType] === 'function') {
+    el[eventType]();
+  } else {
+    const event =
+      eventType === 'string'
+        ? new Event(eventType, {bubbles: true})
+        : eventType;
+    el.dispatchEvent(event);
+  }
+}
+
+trigger(el, 'focus');
 // For a full list of event types: https://developer.mozilla.org/en-US/docs/Web/API/Event
-const event = new Event('change', {bubbles: true});
-el.dispatchEvent(event);
+trigger(el, new PointerEvent('pointerover'));
