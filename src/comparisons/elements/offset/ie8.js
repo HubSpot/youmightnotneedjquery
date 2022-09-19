200c9ce7e9f2032d02bd1a38de0a78e8b362de6e
Peter Abbondanzo <peter@abbondanzo.com>
2022-09-18 23:47:00 -0400

    Fix IE8 offset calculation (#299)
    * Fix IE8 offset calculation
    
    * Subtract client position from offset calculation

--- src/comparisons/elements/offset/ie8.js
+++ src/comparisons/elements/offset/ie8.js
@@ -1,7 +1,14 @@
 function offset(el) {
+  var docElem = document.documentElement;
   var rect = el.getBoundingClientRect();
   return {
-    top: rect.top + document.body.scrollTop,
-    left: rect.left + document.body.scrollLeft
+    top:
+      rect.top +
+      (window.pageYOffset || docElem.scrollTop) -
+      (docElem.clientTop || 0),
+    left:
+      rect.left +
+      (window.pageXOffset || docElem.scrollLeft) -
+      (docElem.clientLeft || 0)
   };
 }
