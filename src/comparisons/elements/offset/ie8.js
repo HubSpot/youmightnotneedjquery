36e00d250bdbe27864d23d9e51a143255cbfd678
Abbondanzo <peter@abbondanzo.com>
2022-09-18 23:43:04 -0400

    Fix IE8 offset calculation

--- src/comparisons/elements/offset/ie8.js
+++ src/comparisons/elements/offset/ie8.js
@@ -1,7 +1,8 @@
 function offset(el) {
+  var docElem = document.documentElement;
   var rect = el.getBoundingClientRect();
   return {
-    top: rect.top + document.body.scrollTop,
-    left: rect.left + document.body.scrollLeft
+    top: rect.top + (window.pageYOffset || docElem.scrollTop),
+    left: rect.left + (window.pageXOffset || docElem.scrollLeft)
   };
 }
