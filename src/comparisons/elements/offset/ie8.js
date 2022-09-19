6514f96ff189f68107fe87ce3d4823b88a870985
Abbondanzo <peter@abbondanzo.com>
2022-09-18 23:45:38 -0400

    Subtract client position from offset calculation

--- src/comparisons/elements/offset/ie8.js
+++ src/comparisons/elements/offset/ie8.js
@@ -2,7 +2,13 @@ function offset(el) {
   var docElem = document.documentElement;
   var rect = el.getBoundingClientRect();
   return {
-    top: rect.top + (window.pageYOffset || docElem.scrollTop),
-    left: rect.left + (window.pageXOffset || docElem.scrollLeft)
+    top:
+      rect.top +
+      (window.pageYOffset || docElem.scrollTop) -
+      (docElem.clientTop || 0),
+    left:
+      rect.left +
+      (window.pageXOffset || docElem.scrollLeft) -
+      (docElem.clientLeft || 0)
   };
 }
