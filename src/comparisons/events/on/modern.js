dad68a8cef263a3f1a1e7d252c43d7413a707579
Abbondanzo <peter@abbondanzo.com>
2023-02-01 01:26:22 -0500

    Ensure the selected target is dispatched as the event target

--- src/comparisons/events/on/modern.js
+++ src/comparisons/events/on/modern.js
@@ -1,15 +1,25 @@
 function addEventListener(el, eventName, eventHandler, selector) {
   if (selector) {
     const wrappedHandler = (e) => {
-      if (e.target && e.target.matches(selector)) {
-        eventHandler(e);
+      if (!e.target) return;
+      const el = e.target.closest(selector);
+      if (el) {
+        const newEvent = Object.create(e, {
+          target: {
+            value: el
+          }
+        });
+        eventHandler.call(el, newEvent);
       }
     };
     el.addEventListener(eventName, wrappedHandler);
     return wrappedHandler;
   } else {
-    el.addEventListener(eventName, eventHandler);
-    return eventHandler;
+    const wrappedHandler = (e) => {
+      eventHandler.call(el, e);
+    };
+    el.addEventListener(eventName, wrappedHandler);
+    return wrappedHandler;
   }
 }
 
